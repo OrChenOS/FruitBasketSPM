4d48d39969361055be239180eda4989ef92f8c93
Or Chen <orchen@pingidentity.com>
2021-08-09 11:36:12 +0300

    fix names

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "FruitBasket",
+    name: "FruitBasketPackage",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "FruitBasket",
-            targets: ["FruitBasket"]),
+            name: "FruitBasketPackage",
+            targets: ["FruitBasketPackage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,6 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.\
-        .binaryTarget(name: "FruitBasket", url: "https://github.com/OrChenOS/SwiftPackasketTest.git", checksum: "09ea82593db595d4cefb3e1d67343da660c74112974f8554774367d2b1c6921c")
+        .binaryTarget(name: "FruitBasketPackage", url: "https://github.com/OrChenOS/SwiftPackasketTest.git", checksum: "09ea82593db595d4cefb3e1d67343da660c74112974f8554774367d2b1c6921c")
     ]
 )
