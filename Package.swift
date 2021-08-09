812d74350a1f74a2e7b9d3565acc1298f8317850
Or Chen <orchen@pingidentity.com>
2021-08-09 11:34:13 +0300

    fix target names

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "FruitBasketPackage",
+    name: "FruitBasket",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "FruitBasketPackage",
-            targets: ["FruitBasketPackage"]),
+            name: "FruitBasket",
+            targets: ["FruitBasket"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
