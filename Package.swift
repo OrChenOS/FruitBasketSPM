c87c5cda842169b513f03b574d62c903d141c600
Or Chen <orchen@pingidentity.com>
2021-08-09 13:41:33 +0300

    replace download url

--- Package.swift
+++ Package.swift
@@ -18,6 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.\
-        .binaryTarget(name: "FruitBasket", url: "https://github.com/OrChenOS/SwiftPackasketTest.git", checksum: "09ea82593db595d4cefb3e1d67343da660c74112974f8554774367d2b1c6921c")
+        .binaryTarget(name: "FruitBasket", url: "https://github.com/OrChenOS/SwiftPackasketTest/raw/main/FruitBasket.xcframework.1.0.0.zip", checksum: "09ea82593db595d4cefb3e1d67343da660c74112974f8554774367d2b1c6921c")
     ]
 )
